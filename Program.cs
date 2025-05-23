68c891da1feacbff80b6945b698a99d9417a3949
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2025-05-23 19:43:56 -0300

    atividade feita

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿﻿Console.WriteLine("--- Horas Extras ---\n");
+
+Console.WriteLine($"salario-hora {12:C}, {300} horas, sendo {0,2} horas-extra, salário = {Salario(12, 300, 0):C}");
+Console.WriteLine($"salario-hora {12:C}, {300} horas, sendo {20} horas-extra, salário = {Salario(12, 300, 20):C}");
+Console.WriteLine($"salario-hora {24:C}, {300} horas, sendo {0,2} horas-extra, salário = {Salario(24, 300, 0):C}");
+Console.WriteLine($"salario-hora {24:C}, {300} horas, sendo {20} horas-extra, salário = {Salario(24, 300, 20):C}");
+
